356b2d638348460d79ccdf0e7356c0f9cd71f566
kurprikitiew <kurprikitiew@gmail.com>
2018-11-04 14:54:15 +0700

    dataObject

---
+++ dataStructureObj.js
@@ -0,0 +1,22 @@
+var user = {
+	nama: "abdul",
+	password: "123"
+}
+
+var database =[ {
+	nama: "abdul",
+	password: "1234" 
+}]
+
+var newsfeed = [{
+		username:"abdul",
+		timeline:"jook"
+	}, 
+	{
+		username: "syukur",
+		timeline: "funny"
+	}, {
+		username:"blackur",
+		timeline:"serah"
+	}
+]
